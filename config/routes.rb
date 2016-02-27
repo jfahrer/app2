7f3f3b06895fb790fff5c50bf6d98542eac0d226
Joshua Chuang <joshua.chuang@gmail.com>
2016-02-27 15:41:43 -0700

    reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :reviews
   resources :events, only: [:index, :show, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227200433) do
+ActiveRecord::Schema.define(version: 20160227214816) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,18 @@ ActiveRecord::Schema.define(version: 20160227200433) do
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id"
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "event_id"
+    t.integer  "user_id"
+    t.integer  "stars"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "reviews", ["event_id"], name: "index_reviews_on_event_id"
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
