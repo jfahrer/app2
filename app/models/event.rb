1a37ef2130a92db02acac232dbda6c960924e46a
Joshua Chuang <joshua.chuang@gmail.com>
2016-02-28 14:13:45 -0700

    make event description optional

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ActiveRecord::Base
   belongs_to :user
 
   validates :name, presence: true
-  validates :description, presence: true
+  # validates :description, presence: true
   validates :price, presence: true, numericality: { integer_only: true,
     greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
   validates :location, presence: true
