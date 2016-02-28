9393669a4e0961388d4fb42f46b23cc2215f2ff5
Joshua Chuang <joshua.chuang@gmail.com>
2016-02-27 18:22:12 -0700

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+users = User.create([
+                        {name: 'Jason', email: 'jason@app2.com', age: 40, sex: 'M'},
+                        {name: 'Dave', email: 'dave@app2.com', age: 33, sex: 'M'},
+                        {name: 'May', email: 'may@app2.com', age: 30, sex: 'F'},
+                        {name: 'Joy', email: 'joy@app2.com', age: 24, sex: 'F'},
+                    ])
+
+events = Event.create!([
+                          {name: 'Taco Tuesday', description: 'Best Taco In Town', category: 'food', price: 1, location: 'JJ Tacon', latitude: 37.7833, longitude: -122.4167, user: users[0], time: Time.now}
+                      ])
+
+reviews = Review.create([
+                            {event: events[0], user: users[0], comment: "so much fun!!", stars: 5}
+                        ] )
