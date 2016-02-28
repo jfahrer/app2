2b756349e104c87056a833f63c2a790eb1a3286b
Joshua Chuang <joshua.chuang@gmail.com>
2016-02-28 14:44:09 -0700

    comment out validation for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,16 +1,16 @@
 class Event < ActiveRecord::Base
   belongs_to :user
 
-  validates :name, presence: true
+  # validates :name, presence: true
   # validates :description, presence: true
-  validates :price, presence: true, numericality: { integer_only: true,
-    greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
-  validates :location, presence: true
-  validates :latitude, presence: true
-  validates :longitude, presence: true
-  validates :user, presence: true
-  validates :category, presence: true
-  validates :time, presence: true
+  # validates :price, presence: true, numericality: { integer_only: true,
+  #   greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  # validates :location, presence: true
+  # validates :latitude, presence: true
+  # validates :longitude, presence: true
+  # validates :user, presence: true
+  # validates :category, presence: true
+  # validates :time, presence: true
 
   has_many :matches
 
